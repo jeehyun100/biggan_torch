343d5a49f3d38dcfa79f44cc15cd76c6cee2be08
PAIK JEE HYUN <intwis100@naver.com>
2021-09-15 05:34:15 +0000

    MODIFY : biggan

--- utils.py
+++ utils.py
@@ -430,7 +430,8 @@ classes_per_sheet_dict = {'I32': 50, 'I32_hdf5': 50,
                           'I64': 50, 'I64_hdf5': 50,
                           'I128': 20, 'I128_hdf5': 20,
                           'I256': 20, 'I256_hdf5': 20,
-                          'C10': 10, 'C100': 100}
+                          'C10': 10, 'C100': 100,
+                          'D256': 4, 'D256_hdf5': 4}
 activation_dict = {'inplace_relu': nn.ReLU(inplace=True),
                    'relu': nn.ReLU(inplace=False),
                    'ir': nn.ReLU(inplace=True),}
